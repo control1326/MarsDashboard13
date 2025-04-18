7ee8658f967afa57230814c5388d1ca9d9821591
Dan Bernard <trashpile132639@gmail.com>
2025-04-18 14:33:02 -0400

    remove example functions.

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -137,33 +137,7 @@ window.addEventListener('load', () => {
 
 
 // Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    //console.log(photodate.getDate(), today.getDate());
-
-    //console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date=== today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-   
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
 
 const RoverLatestPhotos = (latest_photos) => {
 
@@ -204,17 +178,7 @@ const RoverLatestPhotos = (latest_photos) => {
 
 
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json()) 
-        .then(apod => updateStore(store, { apod }))
-        
-
-    //return data
-}
 
 const showMenu = (rovers)=>
     {
